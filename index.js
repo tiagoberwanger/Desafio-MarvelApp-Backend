25af7b309313ec39a606da4a257bc2c67baf3dac
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-08 12:25:44 -0300

    adding cors to req from frontend

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const MarvelController = require('./src/controllers/MarvelController')
 const UserController = require('./src/controllers/UserController')
 const LoginController = require('./src/controllers/LoginController')
 const port = process.env.PORT || 3001
+
+app.use(cors());
 
 app.use(express.json());
 
