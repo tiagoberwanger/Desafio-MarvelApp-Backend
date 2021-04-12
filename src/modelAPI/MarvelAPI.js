cb768a096d8418761ccd477be25619c148620ac7
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-11 21:31:09 -0300

    ajusting limit of results API

--- src/modelAPI/MarvelAPI.js
+++ src/modelAPI/MarvelAPI.js
@@ -4,26 +4,26 @@ const { hash, ts, publicKey } = require('../auth/hashAPI')
 const hashResult = hash();
 
 const allCharacters = async () =>{
-  const characters = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const characters = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json());
   console.log(characters)
   return characters.data.results;
 }
 
 const characterById = async (id) =>{
-  const character = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const character = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=1&ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json())
   return character.data.results;
 }
 
 const allComics = async () =>{
-  const comics = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const comics = await fetch(`https://gateway.marvel.com:443/v1/public/comics?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json());
   return comics.data.results;
 }
 
 const comicById = async (id) =>{
-  const comic = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const comic = await fetch(`https://gateway.marvel.com:443/v1/public/comics?limit=1&ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json())
   console.log(comic)
   return comic.data.results;
