4c1dcbe948cb9d60fd41604e8ebca47e1fadef3f
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-11 21:19:59 -0300

    sending env vars to heroku

--- src/auth/hashAPI.js
+++ src/auth/hashAPI.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
+require('dotenv').config();
 
 const md5 = (keys) => crypto.createHash('md5').update(keys).digest('hex');
-const ts = 1;
-const privateKey = '003cdef8d94e3982ce7d6316de95bb6d6f9c0ee1';
-const publicKey = '95418ebca9cf702b7f32d48ad8d102ec';
+const ts = process.env.TS;
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
 
 const hash = () => md5(ts+privateKey+publicKey);
 
--- src/modelAPI/MarvelAPI.js
+++ src/modelAPI/MarvelAPI.js
@@ -1,28 +1,29 @@
 const fetch = require('node-fetch');
-// const { hash, ts, publicKey } = require('../auth/hashAPI')
+const { hash, ts, publicKey } = require('../auth/hashAPI')
 
-// const hashResult = JSON.stringify(hash());
+const hashResult = hash();
 
 const allCharacters = async () =>{
-  const characters = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=95418ebca9cf702b7f32d48ad8d102ec&hash=24974394572ad39cae74e4145bacde78`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const characters = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json());
+  console.log(characters)
   return characters.data.results;
 }
 
 const characterById = async (id) =>{
-  const character = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=95418ebca9cf702b7f32d48ad8d102ec&hash=24974394572ad39cae74e4145bacde78&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const character = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json())
   return character.data.results;
 }
 
 const allComics = async () =>{
-  const comics = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=95418ebca9cf702b7f32d48ad8d102ec&hash=24974394572ad39cae74e4145bacde78`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const comics = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hashResult}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json());
   return comics.data.results;
 }
 
 const comicById = async (id) =>{
-  const comic = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=95418ebca9cf702b7f32d48ad8d102ec&hash=24974394572ad39cae74e4145bacde78&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
+  const comic = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hashResult}&id=${id}`,{ method: "GET", headers:{ "Content-Type": "application/json"}})
   .then((response)=>response.json())
   console.log(comic)
   return comic.data.results;
