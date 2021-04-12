bff529862e69274a7057cb6eb5f4c4e7ce8025ef
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-12 01:13:44 -0300

    ajusting env vars to test local after deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,70 +1,72 @@
 require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'mysql',her
-//   },
-//   test: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'mysql',
-//   },
-//   production: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'mysql',
-//   },
-// };
-
+//teste local
 module.exports = {
   development: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+    dialect: 'mysql',
   },
   test: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+    dialect: 'mysql',
   },
   production: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+    dialect: 'mysql',
   },
 };
+
+//teste no Heroku
+// module.exports = {
+//   development: {
+//     username: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'postgres',
+//     use_env_variable: 'DATABASE_URL',
+//     dialectOptions: {
+//       "ssl": {
+//         "require": true,
+//         "rejectUnauthorized": false
+//       }
+//     }
+//   },
+//   test: {
+//     username: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'postgres',
+//     use_env_variable: 'DATABASE_URL',
+//     dialectOptions: {
+//       "ssl": {
+//         "require": true,
+//         "rejectUnauthorized": false
+//       }
+//     }
+//   },
+//   production: {
+//     username: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'postgres',
+//     use_env_variable: 'DATABASE_URL',
+//     dialectOptions: {
+//       "ssl": {
+//         "require": true,
+//         "rejectUnauthorized": false
+//       }
+//     }
+//   },
+// };
