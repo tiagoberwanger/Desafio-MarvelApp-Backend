4c80907808ce813260546264a3ca5b64efbd7b32
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-14 14:01:37 -0300

    ajusting config to deploy at heroku

--- src/config/config.js
+++ src/config/config.js
@@ -25,48 +25,48 @@ module.exports = {
   },
 };
 
-//teste no Heroku
-// module.exports = {
-//   development: {
-//     username: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'postgres',
-//     use_env_variable: 'DATABASE_URL',
-//     dialectOptions: {
-//       "ssl": {
-//         "require": true,
-//         "rejectUnauthorized": false
-//       }
-//     }
-//   },
-//   test: {
-//     username: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'postgres',
-//     use_env_variable: 'DATABASE_URL',
-//     dialectOptions: {
-//       "ssl": {
-//         "require": true,
-//         "rejectUnauthorized": false
-//       }
-//     }
-//   },
-//   production: {
-//     username: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOSTNAME,
-//     dialect: 'postgres',
-//     use_env_variable: 'DATABASE_URL',
-//     dialectOptions: {
-//       "ssl": {
-//         "require": true,
-//         "rejectUnauthorized": false
-//       }
-//     }
-//   },
-// };
+teste no Heroku
+module.exports = {
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  },
+  production: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  },
+};
