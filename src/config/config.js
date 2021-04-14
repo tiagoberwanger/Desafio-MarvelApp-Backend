c2a6ef0bf4f65aaf2ad73822b6e45bd2a0af9ac9
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-14 14:01:53 -0300

    ajusting config to deploy at heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 
 //teste local
-module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-};
+// module.exports = {
+//   development: {
+//     username: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'mysql',
+//   },
+//   test: {
+//     username: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'mysql',
+//   },
+//   production: {
+//     username: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.DATABASE,
+//     host: process.env.HOSTNAME,
+//     dialect: 'mysql',
+//   },
+// };
 
 teste no Heroku
 module.exports = {
